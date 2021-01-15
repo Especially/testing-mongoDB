85b73c4911eed8547958bf9bf04de9f32a708397
Especially <_j@live.ca>
2021-01-15 02:37:02 -0500

    Password validation

--- conf/schema/schema.js
+++ conf/schema/schema.js
@@ -1,53 +1,102 @@
 // It was at this point where I realized that I was in a dilemma.
 // The tutorial i was watching was actually using mongoose! and not mongodb, but i had already set up my code, and it was working,
 // but the way that they set up their schema's made it look so fun and exciting, but... since I was already using mongodb, i decided to make my own validation
-// and use classes more in my code, classes are fun!
+// and use classes more in my code, classes are fun and i need to brush up on them, clearly!!
 class userSchema {
     constructor(payload) {
         this.err = [];
         this.strict = true;
+        this.aggregateErr = true;
         this.render = this.render.bind(this);
-        this.email = this.validate(payload.email, { type: 'string', name: 'email', required: true, aggregateErr: true });
-        this.name = this.validate(payload.name, { type: 'string', name: 'name', required: true, aggregateErr: true });
-        this.ip = this.validate(payload.ip, { type: 'number', name: 'ip', aggregateErr: true });
-        this.id = this.validate(payload.id, { type: 'string', name: 'id', required: true, aggregateErr: true });
+        this.data = {
+            email: this.validate(payload.email, { type: 'string', name: 'email', required: true,}),
+            password: this.validate(payload.password, { type: 'string', name: 'password', required: true,}),
+            name: this.validate(payload.name, { type: 'string', name: 'name', required: true,}),
+            ip: this.validate(payload.ip, { type: 'string', name: 'ip',}),
+            created: this.validate(payload.timestamp, { type: 'timestamp', bypass: true, name: 'timestamp',}),
+            // id: this.validate(payload.id, { type: 'string', name: 'id', required: true,}), Pointless, mongo creates this for us
+        }
 
     }
 
     validate(item, options) {
+        //Regex option
+        // Required Check
+
+        // Trim our strings
+        if (options.type === 'string') {
+            item = item.trim();
+        }
+
+        // Type check
+        if (options.type === 'timestamp') {
+            let getTimestamp = (num) => new Date(num).getTime();
+
+            if (isNaN(getTimestamp(item))) { // If bypass, try to resolve error on the server side
+                if (options.bypass) item = new Date().getTime();
+            }
+            if (isNaN(getTimestamp(item))) {
+                if (this.aggregateErr) {
+                    this.err.push({ item: options.name, msg: `Expected a(n) ${options.type}, received ${typeof (item)} ${item}` });
+                }
+            }
+
+        } else {
+            // Default behaviour, if type doesn't match
+            if (typeof (item) !== options.type) {
+                if (this.aggregateErr) {
+                    this.err.push({ item: options.name, msg: `Expected a(n) ${options.type}, received ${typeof (item)}` });
+                }
+            }
+        }
+
         if (options.required) {
+
             if (!item) {
-                if (options.aggregateErr) {
+                if (this.aggregateErr) { // If no aggregation throw on first error we see with that specific error rather than an arr.
                     this.err.push({ item: options.name, msg: 'Cannot be empty' });
                 }
             }
         }
-        if (typeof (item) !== options.type) {
-            if (options.aggregateErr) {
-                this.err.push({ item: options.name, msg: `Expected a(n) ${options.type}, received ${typeof (item)}` });
-            }
+
+        // Regex
+        if (options.regex) {
+            // Password regex will be separate, utilize type:password to use password utility functions
         }
 
         return item
     }
 
-    res = () => {
+    render = () => {
         let responseObj = this;
-
+        let msg;
 
         if (responseObj.err.length > 0) {
-
+            // Strict mode allows me to throw an error response with JUST the error message, otherwise we'll receive the whole class obj
+            // Allows testing with errors
+            if (this.strict) {
+                msg = responseObj.err
+            } else {
+                msg = { msg: responseObj.err, payload: responseObj.data }
+            }
+            responseObj = {
+                success: false,
+                msg
+            }
         } else {
-
+            msg = responseObj.data;
+            responseObj = {
+                success: true,
+                payload: msg
+            }
         }
 
-    }
-
-    render = () => {
-        return ((this.err.length > 0) ? this.err : this);
+        return responseObj;
     }
 }
 
 
-console.log(new userSchema({ id: 'a', name: 'a', email: 'a', ip: 'a' }).render())
-console.log(new userSchema({ id: 'a', name: 'a', email: 'a', ip: 5 }).render())
+// console.log(new userSchema({ id: 'a', name: 'a', email: 'a', ip: 'a' }).render())
+// console.log(new userSchema({ id: 'a', name: 'a', email: 'a', ip: 5 }).render())
+
+module.exports.userSchema = userSchema;
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 // My Playground
+// Note: This application is simply me playing around with mongodb, so if someone is reading this... don't judge me too hard pls
 // Imports
 const express = require('express');
+const { userSchema } = require('./conf/schema/schema');
 const { database } = require('./lib/utils');
 const app = express();
 
@@ -11,7 +13,13 @@ app.use('/api/register', register);
 app.use('/api/login', register);
 //Demo user id: '5ffe3659d8c1e740605b5c19', 
 //Next, add user with schema, then this will be ran in our specified route
-database.addUser({name: 'Barbara Streisand(sp?)',email:'12345@me.ca',password:'1234',ip:'99.237.255.254'});
+let newUser = new userSchema({name: 'Barbara Streisand(sp?)',email:'12345@me.ca',password:'1234',ip:'99.237.255.254',timestamp:''}).render();
+if (newUser.success) {
+    console.log('Success, ', newUser)
+} else {
+    console.log('Failure, ', newUser)
+}
+// database.addUser({name: 'Barbara Streisand(sp?)',email:'12345@me.ca',password:'1234',ip:'99.237.255.254'});
 
 app.listen(8080, () => {
     console.log('Server has started on port 8080.')
