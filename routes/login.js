19a0a59c57e5299bc673e0cb6650d87222d76e56
Especially <_j@live.ca>
2021-02-13 19:08:13 -0500

    :

--- routes/login.js
+++ routes/login.js
@@ -7,13 +7,13 @@ Router.post('/', (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
 
-    if (email && password) {
+    if (email && password) { // Do thorough check of vars in our function
         new database().logIn({email, password})
         .then(result => {
             res.status(201).json(result)
         })
         .catch(err => {
-            res.status(404).json(err)
+            res.status(401).json(err)
         })
     } else {
         res.status(404).json({
